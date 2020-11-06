f480146be2878c872f557b3464f45ab596eddddf
Ramya.U <ramyayadav2016@gmail.com>
2020-11-06 15:01:01 +0530

    [Ramya] Add . Added Append method to add elements

---
+++ LinkedListDataStructures/LinkedList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListDataStructures
+{
+    class LinkedList
+    {
+        internal Node head;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedList"/> class.
+        /// </summary>
+        public LinkedList()
+        {
+            this.head = null;
+        }
+
+        /// UC 2
+        /// <summary>
+        /// Adds the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        internal void Add(int item)
+        {
+            Node node = new Node(item);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+            }
+        }
+
+        /// UC 3
+        /// <summary>
+        /// Appends the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        internal void Append(int item)
+        {
+            Node node = new Node(item);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- LinkedListDataStructures/Program.cs
+++ LinkedListDataStructures/Program.cs
@@ -7,6 +7,15 @@ namespace LinkedListDataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Linked List!");
+            //creating a list
+            LinkedList list = new LinkedList();
+            //adding item to the list
+            list.Add(56);
+            //appending items to the list
+            list.Append(30);
+            list.Append(70);
+            //linkedlist sequence = 56->30->70
+            list.Display();
         }
     }
 }
