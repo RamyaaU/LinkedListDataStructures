0454b53cd37443d5d56c5efb973a4433d06a1719
Ramya.U <ramyayadav2016@gmail.com>
2020-11-06 15:27:45 +0530

    [Ramya] Add . Added method to insert elements at a particular position

---
+++ LinkedListDataStructures/LinkedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListDataStructures
+{
+    class LinkedList
+    {
+        internal Node head;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedList"/> class.
+        /// </summary>
+        public LinkedList()
+        {
+            this.head = null;
+        }
+
+        /// UC 2
+        /// <summary>
+        /// Adds the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        internal void Add(int item)
+        {
+            Node node = new Node(item);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+            }
+        }
+
+        /// UC 3
+        /// <summary>
+        /// Appends the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        internal void Append(int item)
+        {
+            Node node = new Node(item);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+
+        internal void Insert(int position, int item)
+        {
+            Node node = new Node(item);
+            if (position < 1)
+                Console.WriteLine("Invalid Position");
+            else if (position == 1)
+            {
+                node.next = head;
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                try
+                {
+                    while (position > 2)
+                    {
+                        temp = temp.next;
+                        position--;
+                    }
+                    node.next = temp.next;
+                    temp.next = node;
+                }
+                catch (NullReferenceException)
+                {
+                    System.Console.WriteLine("Position out of range");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- LinkedListDataStructures/Program.cs
+++ LinkedListDataStructures/Program.cs
@@ -7,6 +7,17 @@ namespace LinkedListDataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Linked List!");
+            //creating a list
+            LinkedList list = new LinkedList();
+            //adding item to the list
+            list.Add(56);
+            //appending item to the list
+            list.Append(70);
+            //inserting item to the list at 2nd position
+            list.Insert(2, 30);
+            list.Insert(4, 80);
+            //linkedlist sequence = 56->30->70->80
+            list.Display();
         }
     }
 }
