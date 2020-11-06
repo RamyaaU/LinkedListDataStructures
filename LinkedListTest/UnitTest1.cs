6907ba7b65c7c81b319dd0c5fb51d49ad7b35508
Ramya.U <ramyayadav2016@gmail.com>
2020-11-06 20:16:11 +0530

    [Ramya] Add  . Added simple linked list

---
+++ LinkedListTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using LinkedListDataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LinkedListTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            LinkedList list = new LinkedList();
+            list.Add(12);
+            list.Add(30);
+            list.Add(56);
+            Assert.IsTrue(list.Search(30));
+        }
+    }
+}
