5af717725cfaf9e0a983d8c90d93040334f4d4fb
Ramya.U <ramyayadav2016@gmail.com>
2020-11-06 14:36:57 +0530

    [Ramya] Add . Added Creation of the linked list

---
+++ LinkedListDataStructures/LinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListDataStructures
+{
+    class LinkedList
+    {
+        internal Node head;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedList"/> class.
+        /// </summary>
+        public LinkedList()
+        {
+            this.head = null;
+        }
+
+        /// UC 2
+        /// <summary>
+        /// Adds the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        internal void Add(int item)
+        {
+            Node node = new Node(item);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+            }
+        }
+
+        /// <summary>
+        /// Displays this instance.
+        /// </summary>
+        internal void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- LinkedListDataStructures/Program.cs
+++ LinkedListDataStructures/Program.cs
@@ -4,9 +4,21 @@ namespace LinkedListDataStructures
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Linked List!");
+            //creating a list
+            LinkedList list = new LinkedList();
+            //adding items to the list
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            //linkedlist sequence = 56->30->70
+            list.Display();
         }
     }
 }
